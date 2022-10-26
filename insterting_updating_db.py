b88d955c4bc0bc1329065e9e67ec2541c5831357
Mateusz Konat <polakwmc@gazeta.pl>
2022-10-26 10:20:04 +0200

    Create less dependent and readable version of add frame

--- insterting_updating_db.py
+++ insterting_updating_db.py
@@ -1,15 +1,9 @@
-import implementating_alter_abc
-import the_base_for_inserting_updating_db as abc_altering_db
+import implementing_alter_abc as imp
+
 from tkinter import ttk, messagebox, END
-from typing import Type
 import tkinter as tk
+from typing import Union
 from enum import Enum, auto
-
-
-class Employee(Enum):
-    CEO = auto()
-    SECRETARY = auto()
-    INTERN = auto()
 
 
 def show_info(widget, pop_up_text: str, static_text: str) -> None:
@@ -21,42 +15,101 @@ def erase(widgets: list) -> None:
     [widget.delete(0, END) for widget in widgets]
 
 
-def add_student(info_label: ttk.Label, add_listbox: tk.Listbox, first_name: str, surname: str, url: str) -> str:
-    # get all indices from the chosen range
-    class_name_idxs = add_listbox.curselection()
-
-    # create student instance
-    student = implementating_alter_abc.NewStudent(first_name, surname, url, class_name_idxs)
-
-    # make sure all parameters are given
-    if not student.data_is_correct:
-        return tk.messagebox.showerror("Error", "You didn't provide all data or provided data was incorrect.")
-
-    student = add_listbox.get(class_name_idxs[0])
-    insert_student(first_name, surname, class_name_values[0], url)
-    # displaying information about adding student (temporarily)
-    show_info(info_label, "Added successfully!", "(Updated/added class name)")
-
-    # erasing entry boxes so the user doesn't have to do it by himself
-    erase([name_entry, surname_entry, url_entry])
+# def add_student(first_name: str, surname: str, url: str, class_name: tuple[str]) -> str:
+#     # create student instance
+#     student = imp.NewStudent(first_name, surname, url, class_name_idxs)
+#
+#     # make sure all parameters are given
+#     if not student.data_is_correct:
+#         return tk.messagebox.showerror("Error", "You didn't provide all data or provided data was incorrect.")
+#
+#     student = add_listbox.get(class_name_idxs[0])
+#     insert_student(first_name, surname, class_name_values[0], url)
+#     # displaying information about adding student (temporarily)
+#     show_info(info_label, "Added successfully!", "(Updated/added class name)")
+#
+#     # erasing entry boxes so the user doesn't have to do it by himself
+#     erase([name_entry, surname_entry, url_entry])
 
 
-def class_chosen(class_listbox: tk.Listbox) -> bool:
-    if not class_listbox.curselection():
-        return False
-    return True
-
-
-def parameters_given(object_to_check: Type[abc_altering_db.AlterDB]) -> bool:
-    if not object_to_check.data_is_correct:
-        return False
-    return True
-
-
-def alter_db(object_to_alter_db: Type[abc_altering_db.AlterDB]) -> None:
+def alter_db(object_to_alter_db: Union[imp.NewStudent, imp.NewClass, imp.UpdateClass]) -> None:
     object_to_alter_db.alter()
     object_to_alter_db.commit()
     object_to_alter_db.close_db()
 
 
-alter_db(implementating_alter_abc.NewStudent)
+class AddMode(Enum):
+    UPDATE_CLASS = auto()
+    NEW_CLASS = auto()
+    NEW_STUDENT = auto()
+
+
+def manage_updating_class_record(old_class_name: tuple[str], new_class_name: str) -> str:
+    updated_class = imp.UpdateClass(old_class_name, new_class_name)
+
+    if not updated_class.data_is_correct:
+        return tk.messagebox.showerror("Error", "Provided data is incorrect!")
+
+    if updated_class.exists_in_db:
+        return tk.messagebox.showerror("Error", "Class of given name already exists!")
+
+    # updating class record
+    alter_db(updated_class)
+
+    return tk.messagebox.showinfo("Completed!", "Class updated successfully!")
+
+
+def manage_inserting_class(class_name: str) -> str:
+    inserted_class = imp.NewClass(class_name)
+
+    if not inserted_class.data_is_correct:
+        return tk.messagebox.showerror("Error", "Provided data is incorrect!")
+
+    if inserted_class.exists_in_db:
+        return tk.messagebox.showerror("Error", "Class of given name already exists!")
+
+    # inserting class into db
+    alter_db(inserted_class)
+
+    return tk.messagebox.showinfo("Completed!", "Class added successfully!")
+
+
+def manage_inserting_student(first_name: str, surname: str,
+                             url: str, class_name: tuple[str]) -> str:
+    inserted_student = imp.NewStudent(first_name, surname, url, class_name)
+
+    if not inserted_student.data_is_correct:
+        return tk.messagebox.showerror("Error", "Provided data is incorrect!")
+
+    # inserting student into db
+    alter_db(inserted_student)
+
+    return tk.messagebox.showinfo("Completed!", "Student added successfully!")
+
+
+def choose_mode_add(mode: AddMode, class_listbox: tk.Listbox,
+                    class_name_entry: ttk.Entry, first_name_entry: ttk.Entry,
+                    surname_entry: ttk.Entry, url_entry: ttk.Entry) -> str:
+    if mode == AddMode.UPDATE_CLASS:
+        if not class_listbox.curselection():
+            return tk.messagebox.showerror("Error", "No class given!")
+
+        old_class_name = class_listbox.get(class_listbox.curselection())
+        new_class_name = class_name_entry.get()
+        manage_updating_class_record(old_class_name, new_class_name)
+    elif mode == AddMode.NEW_CLASS:
+        class_name = class_name_entry.get()
+        manage_inserting_class(class_name)
+    elif mode == AddMode.NEW_STUDENT:
+        if not class_listbox.curselection():
+            return tk.messagebox.showerror("Error", "No class given!")
+
+        first_name = first_name_entry.get()
+        surname = surname_entry.get()
+        url = url_entry.get()
+        class_name = class_listbox.get(class_listbox.curselection())
+
+        manage_inserting_student(first_name, surname, url, class_name)
+
+    # erasing values from widgets: change state if needed, come back to the previous state,
+    # update class_listbox after altering classes
