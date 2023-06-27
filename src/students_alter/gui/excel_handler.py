b02605fa76f5f3ec24aa50e87b8395cea825f8f6
Mateusz Konat <polakwmc@gazeta.pl>
2023-06-27 16:39:10 +0200

    Add Excel Handler

--- src/students_alter/gui/excel_handler.py
+++ src/students_alter/gui/excel_handler.py
@@ -1,43 +1,72 @@
 import tkinter as tk
 from tkinter import ttk, simpledialog, messagebox, filedialog
 
+from pathlib import Path
+
 from openpyxl import Workbook
+
+PAD_X = 10
+PAD_Y = 10
 
 
 class ExcelHandler(ttk.Frame):
     """Manages the creation and addition of Excel files"""
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(self, *args, **kwargs)
+    def __init__(self, root, *args, **kwargs):
+        super().__init__(root, *args, **kwargs)
 
         self.create_spreadsheet_button = ttk.Button(self,
                                                     text="Create Spreadsheet",
-                                                    command=lambda e: self.create_spreadsheet()
+                                                    command=self.create_spreadsheet
                                                     )
 
-        self.load_spreadsheet_button = ttk.Button(self, text="Load Spreadsheet")
+        self.load_spreadsheet_button = ttk.Button(self,
+                                                  text="Load Spreadsheet",
+                                                  command=self.load_spreadsheet
+                                                  )
 
-        self.spreadsheet_path_entry_var = tk.StringVar()
-        self.spreadsheet_path_entry = ttk.Entry(self, textvariable=self.spreadsheet_path_entry_var)
+        self.spreadsheet_path_entry = ttk.Entry(self)
+        self.spreadsheet_path_label = ttk.Label(self, text="Spreadsheet path:")
+
+        self.position_entries_and_labels()
+
+        self.columnconfigure(index=0, weight=1)
+        self.columnconfigure(index=1, weight=1)
+        self.rowconfigure(index=0, weight=1)
+        self.rowconfigure(index=1, weight=1)
 
     @staticmethod
     def create_spreadsheet():
         class_name = tk.simpledialog.askstring(title="Create Spreadsheet", prompt="Type class name:")
-        workbook = Workbook()
-        sheet = workbook.active
+        if class_name is not None:
+            workbook = Workbook()
+            sheet = workbook.active
 
-        sheet["A1"] = "first_name"
-        sheet["B1"] = "surname"
-        sheet["C1"] = "url"
-        sheet["E1"] = class_name
+            sheet["A1"] = "first_name"
+            sheet["B1"] = "surname"
+            sheet["C1"] = "url"
+            sheet["E1"] = class_name
 
-        workbook.save(filename=class_name)
+            class_name_dir = Path(__name__).resolve().parent.parent.parent.parent.parent
+            workbook.save(filename=class_name_dir.joinpath(f"{class_name}.xlsx"))
 
-        return tk.messagebox.showinfo("Spreadsheet creation status", "Spreadsheet created successfully!")
+            message = "Spreadsheet created successfully!"
+        else:
+            message = "Failed to create spreadsheet."
+        info = "Spreadsheet creation status"
+
+        return tk.messagebox.showinfo(info, message)
 
     def load_spreadsheet(self):
         filename = filedialog.askopenfilename(initialdir="/",
                                               title="Select a File",
                                               filetypes=(("Excel files", "*.xlsx"),)
                                               )
-        self.spreadsheet_path_entry_var = filename
+        self.spreadsheet_path_entry.delete(0, tk.END)
+        self.spreadsheet_path_entry.insert(0, filename)
+
+    def position_entries_and_labels(self):
+        self.create_spreadsheet_button.grid(column=1, row=0, padx=PAD_X, pady=PAD_Y, sticky=tk.EW)
+        self.load_spreadsheet_button.grid(column=0, row=0, padx=PAD_X, pady=PAD_Y, sticky=tk.EW)
+        self.spreadsheet_path_entry.grid(column=1, row=1, padx=PAD_X, pady=PAD_Y, sticky=tk.EW)
+        self.spreadsheet_path_label.grid(column=0, row=1, padx=PAD_X, pady=PAD_Y, sticky=tk.E)
--- src/students_alter/gui/modes_frames.py
+++ src/students_alter/gui/modes_frames.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 
-from src.students_alter.gui import data_entries
+from src.students_alter.gui import data_entries, excel_handler
 
 PAD_X = 10
 PAD_Y = 10
@@ -21,6 +21,8 @@ class ModesNotebook(ttk.Notebook):
         self.add(self.add_student, text="Add student")
         self.update_student = ttk.Frame(self)
         self.add(self.update_student, text="Update student")
+        self.handle_excel = ttk.Frame(self)
+        self.add(self.handle_excel, text="Add multiple students")
 
         # Give weight to Tabs
         self.give_weight()
@@ -36,6 +38,9 @@ class ModesNotebook(ttk.Notebook):
         self.students_entries_update = data_entries.StudentEntriesFrame(self.update_student)
         self.students_entries_update.grid(row=0, column=0, sticky=tk.NSEW)
 
+        self.handle_excel_tab = excel_handler.ExcelHandler(self.handle_excel)
+        self.handle_excel_tab.grid(row=0, column=0, sticky=tk.NSEW)
+
     def give_weight(self):
         self.add_class.columnconfigure(index=0, weight=1)
         self.add_class.rowconfigure(index=0, weight=1)
@@ -48,6 +53,9 @@ class ModesNotebook(ttk.Notebook):
 
         self.update_student.columnconfigure(index=0, weight=1)
         self.update_student.rowconfigure(index=0, weight=1)
+
+        self.handle_excel.columnconfigure(index=0, weight=1)
+        self.handle_excel.rowconfigure(index=0, weight=1)
 
     def return_current_tab(self):
         return self.index(self.select())
