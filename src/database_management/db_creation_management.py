5c418382ed4dfb1a6fc25dadbf864fc643e940be
Mateusz Konat <polakwmc@gazeta.pl>
2023-08-07 13:02:50 +0200

    Give context manager for creating db

--- src/database_management/db_creation_management.py
+++ src/database_management/db_creation_management.py
@@ -1,30 +1,25 @@
-import sqlite3
-
-DATABASE = "cas_portfolios.db"
+from src.database_management.db_manager import SQLite
 
 
-def create_database(database_name):
-    conn = sqlite3.connect(database_name)
-    cursor = conn.cursor()
+def create_database():
 
-    cursor.execute("PRAGMA foreign_keys = ON")
+    with SQLite() as cur:
+        cur.execute("PRAGMA foreign_keys = ON")
 
-    cursor.execute("""CREATE TABLE IF NOT EXISTS classes(
-                    class_id INTEGER PRIMARY KEY,
-                    class_name TEXT NOT NULL UNIQUE
-                    );""")
+        cur.execute("""CREATE TABLE IF NOT EXISTS classes(
+                        class_id INTEGER PRIMARY KEY,
+                        class_name TEXT NOT NULL UNIQUE
+                        );""")
 
-    cursor.execute("""CREATE TABLE IF NOT EXISTS students(
-                    student_id INTEGER PRIMARY KEY,
-                    first_name TEXT NOT NULL,
-                    surname TEXT NOT NULL COLLATE NOCASE,
-                    class_id INTEGER,
-                    url TEXT NOT NULL,
-                    FOREIGN KEY (class_id) REFERENCES classes(class_id)
-                    ON DELETE CASCADE);""")
-
-    conn.close()
+        cur.execute("""CREATE TABLE IF NOT EXISTS students(
+                        student_id INTEGER PRIMARY KEY,
+                        first_name TEXT NOT NULL,
+                        surname TEXT NOT NULL COLLATE NOCASE,
+                        class_id INTEGER,
+                        url TEXT NOT NULL,
+                        FOREIGN KEY (class_id) REFERENCES classes(class_id)
+                        ON DELETE CASCADE);""")
 
 
 if __name__ == "__main__":
-    create_database(DATABASE)
+    create_database()
